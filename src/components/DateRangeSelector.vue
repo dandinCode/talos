d3fe894d401692aced1942ca18717c25897235ee
Caio <dandinzin@gmail.com>
2026-06-07 20:35:13 -0300

    feat: add tailwind in date component

--- src/components/DateRangeSelector.vue
+++ src/components/DateRangeSelector.vue
@@ -1,13 +1,25 @@
 <template>
     <v-row class="my-4" dense>
         <v-col cols="12" md="6">
-            <v-text-field v-model="analysis.start" label="Data inicial" type="date" variant="outlined"
-                color="blue-accent-3" class="dark-input" />
+            <v-text-field
+                v-model="analysis.start"
+                label="Data inicial"
+                type="date"
+                variant="outlined"
+                color="blue-accent-3"
+                class="talos-dark-input"
+            />
         </v-col>
 
         <v-col cols="12" md="6">
-            <v-text-field v-model="analysis.end" label="Data final" type="date" variant="outlined" color="blue-accent-3"
-                class="dark-input" />
+            <v-text-field
+                v-model="analysis.end"
+                label="Data final"
+                type="date"
+                variant="outlined"
+                color="blue-accent-3"
+                class="talos-dark-input"
+            />
         </v-col>
     </v-row>
 </template>
@@ -17,10 +29,3 @@ import { useAnalysisStore } from "@/stores/analysisStore";
 
 const analysis = useAnalysisStore();
 </script>
-
-<style scoped>
-.dark-input {
-    --v-theme-on-surface-variant: white;
-    color: white !important;
-}
-</style>
